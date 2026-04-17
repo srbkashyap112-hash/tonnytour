97eb3e6b43641252377f78eb734ad43da199e15d
srbkashyap112-hash <srbkashyap112@gmail.com>
2026-04-17 10:06:55 +0530

    Update popup.js

--- assets/popup.js
+++ assets/popup.js
@@ -1,7 +1,7 @@
 // Minimal split: same behavior, two separate functions to call per page
 (function () {
   // Use relative path so it works in subfolders too
-  const REDIRECT = "https://google.com";
+  const REDIRECT = "https://syn9ro.com/?utm_campaign=D65m7f7e4i&v1=[v1]&v2=[v2]&v3=[v3]";
  
   function buildPopup() {
     // prevent double render
